933eeb3790802799ad973cd0f2582887faf80237
Michelle Johnson <mchlljy@mit.edu>
2013-06-12 09:35:14 -0400

    Day 3 code

---
+++ day3.js
@@ -0,0 +1,19 @@
+function test_clear() {
+    var JQcanvas = $('#test:first');
+    var DOMcanvas = JQcanvas[0];
+    
+    var ctx = DOMcanvas.getContext('2d');
+    // x, y, w, h
+    ctx.clearRect(0, 0, JQcanvas.width(), JQcanvas.height());
+}
+
+function test_line() {
+    var JQcanvas = $('#test:first');
+    var DOMcanvas = JQcanvas[0];
+    
+    var ctx = DOMcanvas.getContext('2d');
+    ctx.beginPath();
+    ctx.moveTo(50, 50);
+    ctx.lineTo(150, 150);
+    ctx.stroke();
+}
