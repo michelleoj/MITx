11258eb683b0eba9d532c0c7ede22f384f3c8c48
Michelle Johnson <mchlljy@mit.edu>
2013-06-19 10:01:54 -0400

    initial version of horizontal bar chart

---
+++ Week 2/Visualizations/visual.js
@@ -0,0 +1,18 @@
+var data = [0, 4, 8, 8, 15, 16, 23, 42];
+
+//var chart = $("<div></div>").addClass("chart");
+//$(".chart-container").append(chart);
+
+////data.forEach(function(d) { chart.append(d) });
+//data.forEach(function(d) {
+//    chart.append($("<div></div>").css("width", d*10 + "px").text(d));
+//});
+
+var xScale = d3.scale.linear()
+    .domain([0, d3.max(data)]).range(["0%", "100%"]);
+
+var chart = d3.select(".chart-container").append("div").attr("class", "chart");
+chart.selectAll("div").data(data)
+    .enter().append("div")
+    .style("width", xScale)
+    .text(function(d) { return d });
